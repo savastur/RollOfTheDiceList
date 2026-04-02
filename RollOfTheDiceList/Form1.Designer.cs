b1565c3f49f68c78b25e2ab42423e5c12bbdab3a
Sturm <savagesturm@gmail.com>
2026-04-02 08:50:53 -0600

    Add project files.

---
+++ RollOfTheDiceList/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace RollOfTheDiceList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RollDisplay = new System.Windows.Forms.RichTextBox();
+            this.RollDice = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // RollDisplay
+            // 
+            this.RollDisplay.AccessibleDescription = "Keeps track of how many time\'s a number is rolled";
+            this.RollDisplay.AccessibleName = "Roll Index";
+            this.RollDisplay.Location = new System.Drawing.Point(21, 12);
+            this.RollDisplay.Name = "RollDisplay";
+            this.RollDisplay.Size = new System.Drawing.Size(723, 296);
+            this.RollDisplay.TabIndex = 0;
+            this.RollDisplay.Text = "";
+            // 
+            // RollDice
+            // 
+            this.RollDice.AccessibleDescription = "Roll two six sided die";
+            this.RollDice.AccessibleName = "Roll Dice";
+            this.RollDice.Location = new System.Drawing.Point(7, 39);
+            this.RollDice.Name = "RollDice";
+            this.RollDice.Size = new System.Drawing.Size(75, 23);
+            this.RollDice.TabIndex = 1;
+            this.RollDice.Text = "Roll Dice";
+            this.RollDice.UseVisualStyleBackColor = true;
+            this.RollDice.Click += new System.EventHandler(this.RollDice_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.AccessibleDescription = "Closes the Program";
+            this.ExitButton.AccessibleName = "Exit Button";
+            this.ExitButton.Location = new System.Drawing.Point(107, 39);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 23);
+            this.ExitButton.TabIndex = 2;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.RollDice);
+            this.groupBox1.Controls.Add(this.ExitButton);
+            this.groupBox1.Location = new System.Drawing.Point(218, 324);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.RollDisplay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox RollDisplay;
+        private System.Windows.Forms.Button RollDice;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ RollOfTheDiceList/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RollOfTheDiceList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static void Display(int[] numberSum)
+        {
+            List<string> rollIndex = new List<string>(); 
+            string[] numberList = { "1", "2", "3", "4", "5", "6", "8", "9", "10", "11", "12" };
+            foreach(string number in numberList)
+            {
+                string numberIndex = number.PadLeft(2) + " |";
+                rollIndex.Add(numberIndex);
+            }
+            foreach(int sum  in numberSum)
+            {
+                string sumIndex = sum.ToString().PadLeft(2) + " |";
+                rollIndex.Add(sumIndex);
+            }
+            
+        }
+
+        private void RollDice_Click(object sender, EventArgs e)
+        {
+            int[] rolls = new int[11];
+            //Random Number Generator
+            Random sixSidedDie = new Random();
+            for (int rollDice = 0; rollDice < 1000; rollDice++)
+            {
+                //Rolls two dice and adds together
+                int sixSidedNumber = sixSidedDie.Next(1, 7) + sixSidedDie.Next(1, 7); //(min , max "not inclusive")
+                //incremnts every time an number is rolled and removes the zero's displayed for 0 and 1
+                rolls[sixSidedNumber - 2]++;
+            }
+            Display(rolls);
+        }
+    }
+}
